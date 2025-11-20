6029d2f80efc7feff344440bfae25ae074a83c70
saijo-0124 <haruki.1026.ab@icloud.com>
2025-11-20 13:58:46 +0900

    Update lecture05_01.py

--- oop2-2025-18-GXX/src/lecture05_01.py
+++ oop2-2025-18-GXX/src/lecture05_01.py
@@ -1,17 +1,24 @@
 import numpy as np
 import cv2
-from my_module.K21999.lecture05_camera_image_capture import MyVideoCapture
+from my_module.K21999.lecture05_camera_image_capture import MyVideoCapture 
 
 def lecture05_01():
 
     # カメラキャプチャ実行
     app = MyVideoCapture()
     app.run()
+    
+
 
     # 画像をローカル変数に保存
     google_img : cv2.Mat = cv2.imread('images/google.png')
-    capture_img : cv2.Mat = cv2.imread('images/camera_capture.png') # 動作テスト用なので提出時にこの行を消すこと
-    # capture_img : cv2.Mat = "implement me"
+
+    capture_img : cv2.Mat = app.get_img()
+
+    g_hight, g_width, g_channel = google_img.shape
+    c_hight, c_width, c_channel = capture_img.shape
+    print(google_img.shape)
+    print(capture_img.shape)
 
     g_hight, g_width, g_channel = google_img.shape
     c_hight, c_width, c_channel = capture_img.shape
@@ -23,9 +30,11 @@ def lecture05_01():
             g, b, r = google_img[y, x]
             # もし白色(255,255,255)だったら置き換える
             if (b, g, r) == (255, 255, 255):
-                pass
-                #implement me
+                google_img[y, x] = capture_img[y % c_hight, x % c_width]
+                
 
     # 書き込み処理
-    # implement me
+    cv2.imwrite('lecture06_01_K24066.png',google_img)
 
+    if __name__=="__main__":
+        lecture05_01()
